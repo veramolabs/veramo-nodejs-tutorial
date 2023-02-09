551ab1e42e9b06866c524772e3f78d2e75654471
nickreynolds <nicholas.s.reynolds@gmail.com>
2023-02-08 20:10:03 -0500

    use esm veramo in commonjs context

--- src/veramo/setup.ts
+++ src/veramo/setup.ts
@@ -1,47 +1,51 @@
-// Core interfaces
-import { createAgent, IDIDManager, IResolver, IDataStore, IKeyManager, ICredentialPlugin } from '@veramo/core'
-
-// Core identity manager plugin
-import { DIDManager } from '@veramo/did-manager'
-
-// Ethr did identity provider
-import { EthrDIDProvider } from '@veramo/did-provider-ethr'
-
-// Web did identity provider
-import { WebDIDProvider } from '@veramo/did-provider-web'
-
-// Core key manager plugin
-import { KeyManager } from '@veramo/key-manager'
-
-// Custom key management system for RN
-import { KeyManagementSystem, SecretBox } from '@veramo/kms-local'
-
-// W3C Verifiable Credential plugin
-import { CredentialPlugin } from '@veramo/credential-w3c'
-
-// Custom resolvers
-import { DIDResolverPlugin } from '@veramo/did-resolver'
-import { Resolver } from 'did-resolver'
-import { getResolver as ethrDidResolver } from 'ethr-did-resolver'
-import { getResolver as webDidResolver } from 'web-did-resolver'
-
-// Storage plugin using TypeOrm
-import { Entities, KeyStore, DIDStore, IDataStoreORM, PrivateKeyStore, migrations } from '@veramo/data-store'
-
-// TypeORM is installed with `@veramo/data-store`
-import { DataSource } from 'typeorm'
-
 // This will be the name for the local sqlite database for demo purposes
 const DATABASE_FILE = 'database.sqlite'
 
 // You will need to get a project ID from infura https://www.infura.io
-const INFURA_PROJECT_ID = '3586660d179141e3801c3895de1c2eba'
+const INFURA_PROJECT_ID = '33aab9e0334c44b0a2e0c57c15302608'
 
 // This will be the secret key for the KMS
 const KMS_SECRET_KEY =
-  '11b574d316903ced6cc3f4787bbcc3047d9c72d1da4d83e36fe714ef785d10c1'
+  'c0710059b687bf53009f7b935903ba48334e569780851008392f3e7f595c347a'
+const { Resolver } = require('did-resolver')
+const { getResolver: ethrDidResolver } = require('ethr-did-resolver')
+const { getResolver: webDidResolver } = require('web-did-resolver')
+import { TAgent, IDIDManager, IResolver, IDataStore, IKeyManager, ICredentialPlugin } from '@veramo/core-types'
 
-  const dbConnection = new DataSource({
+async function getAgent(): Promise<TAgent<IDIDManager & IKeyManager & IDataStore & IResolver & ICredentialPlugin>> {
+  // Core interfaces
+  const { createAgent } = await import('@veramo/core')
+
+  // Core identity manager plugin
+  const { DIDManager } = await import('@veramo/did-manager')
+
+  // Ethr did identity provider
+  const { EthrDIDProvider } = await import('@veramo/did-provider-ethr')
+
+  // Web did identity provider
+  const { WebDIDProvider } = await import('@veramo/did-provider-web')
+
+  // Core key manager plugin
+  const { KeyManager } = await import('@veramo/key-manager')
+
+  // Custom key management system for RN
+  const { KeyManagementSystem, SecretBox } = await import('@veramo/kms-local')
+
+  // W3C Verifiable Credential plugin
+  const { CredentialPlugin } = await import('@veramo/credential-w3c')
+
+  // Custom resolvers
+  const { DIDResolverPlugin } = await import('@veramo/did-resolver')
+
+
+  // Storage plugin using TypeOrm
+  const { Entities, KeyStore, DIDStore, PrivateKeyStore, migrations } = await import('@veramo/data-store')
+
+  // TypeORM is installed with `@veramo/data-store`
+  const { createConnection } = await import('typeorm')
+
+  
+  const dbConnection = createConnection({
     type: 'sqlite',
     database: DATABASE_FILE,
     synchronize: false,
@@ -49,11 +53,12 @@ const KMS_SECRET_KEY =
     migrationsRun: true,
     logging: ['error', 'info', 'warn'],
     entities: Entities,
-  }).initialize()
+    })
 
-  export const agent = createAgent<IDIDManager & IKeyManager & IDataStore & IDataStoreORM & IResolver & ICredentialPlugin>({
+  const agent = createAgent<IDIDManager & IKeyManager & IDataStore & IResolver & ICredentialPlugin>({
     plugins: [
       new KeyManager({
+        // @ts-ignore
         store: new KeyStore(dbConnection),
         kms: {
           local: new KeyManagementSystem(new PrivateKeyStore(dbConnection, new SecretBox(KMS_SECRET_KEY))),
@@ -61,12 +66,12 @@ const KMS_SECRET_KEY =
       }),
       new DIDManager({
         store: new DIDStore(dbConnection),
-        defaultProvider: 'did:ethr:goerli',
+        defaultProvider: 'did:ethr:rinkeby',
         providers: {
-          'did:ethr:goerli': new EthrDIDProvider({
+          'did:ethr:rinkeby': new EthrDIDProvider({
             defaultKms: 'local',
-            network: 'goerli',
-            rpcUrl: 'https://goerli.infura.io/v3/' + INFURA_PROJECT_ID,
+            network: 'rinkeby',
+            rpcUrl: 'https://rinkeby.infura.io/v3/' + INFURA_PROJECT_ID,
           }),
           'did:web': new WebDIDProvider({
             defaultKms: 'local',
@@ -82,3 +87,6 @@ const KMS_SECRET_KEY =
       new CredentialPlugin(),
     ],
   })
+  return agent
+}
+module.exports.getAgent = getAgent
